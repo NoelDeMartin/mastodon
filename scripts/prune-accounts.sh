82977d9a91d5e5609df183a361ae2dbb091fcf48
Noel De Martin <noel@noeldemartin.com>
2025-01-08 13:22:07 +0100

    Update script logs

--- scripts/prune-accounts.sh
+++ scripts/prune-accounts.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env sh
 
-# Workaround for a missing feature to avoid caching remote assets
+# Workaround for removing cached remote assets
 # See https://github.com/mastodon/mastodon/issues/15195
 
 # sudo crontab -e
 # 0 5 * * 1 /var/www/mastodon/prune-data.sh >> /var/log/cron-mastodon.log 2>> /var/log/cron-mastodon.log
+
+echo "[`date`] Pruning mastodon accounts..."
 
 # Delete cached assets
 rm /var/www/mastodon/public/system/cache -rf
@@ -19,3 +21,6 @@ docker compose -f /var/www/mastodon/docker-compose.yml exec -T web tootctl accou
 docker compose -f /var/www/mastodon/docker-compose.yml exec -T web tootctl accounts refresh aral@mastodon.ar.al
 docker compose -f /var/www/mastodon/docker-compose.yml exec -T web tootctl accounts refresh nolan@toot.cafe
 docker compose -f /var/www/mastodon/docker-compose.yml exec -T web tootctl accounts refresh tychi@merveilles.town
+
+echo "[`date`] Mastodon accounts pruned!"
+echo ""
