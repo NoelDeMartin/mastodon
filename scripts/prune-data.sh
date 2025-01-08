4fc4606aa4c5c5e10161d317f40daad98f7a9d33
Noel De Martin <noel@noeldemartin.com>
2025-01-08 10:13:15 +0100

    Prune data more aggressively

---
+++ scripts/prune-data.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+# crontab -e
+# 30 5 * * 1 /var/www/mastodon/prune-data.sh >> /var/log/cron-mastodon.log 2>> /var/log/cron-mastodon.log
+
+echo "[`date`] Pruning mastodon data..."
+docker compose -f /var/www/mastodon/docker-compose.yml exec -T web tootctl statuses remove
+docker compose -f /var/www/mastodon/docker-compose.yml exec -T web tootctl accounts prune
+docker compose -f /var/www/mastodon/docker-compose.yml exec -T web tootctl emoji purge --remote-only
+docker compose -f /var/www/mastodon/docker-compose.yml exec -T web tootctl preview_cards remove
+docker compose -f /var/www/mastodon/docker-compose.yml exec -T web tootctl media remove
+docker compose -f /var/www/mastodon/docker-compose.yml exec -T web tootctl media remove --prune-profiles
+docker compose -f /var/www/mastodon/docker-compose.yml exec -T web tootctl media remove-orphans
+
+echo "[`date`] Mastodon data pruned!"
+echo ""
