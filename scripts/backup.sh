3c6c7d25f1507bdeea815ea981915237bb121955
Noel De Martin <noel@noeldemartin.com>
2025-01-17 08:15:15 +0100

    Fix backing up redis database
    The previous command wasn't working due to wrong permissions

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -9,10 +9,10 @@ echo "Backing up database..."
 docker compose -f $basedir/docker-compose.yml exec db pg_dump -U postgres postgres > $RIREKI_BACKUP_PATH/dump.sql
 echo "Database backed up!"
 
+echo "Backing up redis..."
+docker compose -f $basedir/docker-compose.yml exec redis cat /data/dump.rdb > $RIREKI_BACKUP_PATH/dump.rdb
+echo "Redis backed up!"
+
 echo "Backing up secrets..."
 cp $basedir/.env.production $RIREKI_BACKUP_PATH/.env.production
 echo "Secrets backed up!"
-
-echo "Backing up redis..."
-cp $basedir/redis/dump.rdb $RIREKI_BACKUP_PATH/dump.rdb
-echo "Redis backed up!"
--- scripts/prepare-permissions.sh
+++ scripts/prepare-permissions.sh
@@ -2,5 +2,4 @@
 
 MASTODON_UID=`docker compose run --rm web id -u mastodon | sed 's/\r$//'`
 
-sudo chmod 666 ./redis/dump.rdb
 sudo chown -R $MASTODON_UID:$MASTODON_UID ./public/system
